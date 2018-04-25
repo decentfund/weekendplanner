6bff1d613421d932191c362f8b28c295288b181e
Igor Line <linesethenoiz@gmail.com>
2018-04-25 15:07:12 +0200

    Replaced LoadingContainer for MetamaskStatus. Closes #3.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { DrizzleProvider } from 'drizzle-react';
-import { LoadingContainer } from 'drizzle-react-components';
+// import { LoadingContainer } from 'drizzle-react-components';
 
 // Layouts
 import App from './App';
 import HomeContainer from './layouts/home/HomeContainer';
+import MetamaskStatus from './user/layouts/metamask/MetamaskStatus';
 
 import store from './store';
 import drizzleOptions from './drizzleOptions';
@@ -17,13 +18,13 @@ const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
   <DrizzleProvider options={drizzleOptions} store={store}>
-    <LoadingContainer>
+    <MetamaskStatus>
       <Router history={history}>
         <Route path="/" component={App}>
           <IndexRoute component={HomeContainer} />
         </Route>
       </Router>
-    </LoadingContainer>
+    </MetamaskStatus>
   </DrizzleProvider>,
   document.getElementById('root'), // eslint-disable-line
 );
