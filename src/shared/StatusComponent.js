adf766331cd3751ed37bf71b8f8c681fd6b991ab
palevoo <kud.nikita@gmail.com>
2018-05-10 18:47:29 +0300

    Added Status Component without styles.

---
+++ src/shared/StatusComponent.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Blockies from 'react-blockies';
+
+const clipper = (address) => {
+  const arr = address.split('');
+  const first = arr.slice(0, 4).join('');
+  const last = arr.slice(arr.length - 4).join('');
+  return `${first}...${last}`;
+};
+
+const StatusComponent = props =>
+  (
+    <div>
+      <Blockies {...props} />
+      <div>{clipper(props.address)}</div>
+      <div>{props.vote}</div>
+      <div>{props.owner}</div>
+    </div>
+  );
+
+StatusComponent.defaultProps = {
+  address: null,
+  vote: null,
+  owner: true,
+};
+
+StatusComponent.propTypes = {
+  address: PropTypes.string,
+  vote: PropTypes.string,
+  owner: PropTypes.string,
+};
+
+export default StatusComponent;
--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Metamask } from '../src/user/layouts/metamask/MetamaskStatus';
 require('./TextField.stories.js');
+require('./StatusComponent.stories.js');
+
 
 storiesOf('Metamask', module)
   .add('accounts not available', () => (
