d395d78d1dd874004fcb017d22afde3510cd39e9
palevoo <kud.nikita@gmail.com>
2018-05-17 14:22:37 +0200

    refactor

--- stories/index.js
+++ stories/index.js
@@ -8,5 +8,7 @@ require('./NewEventAttendees.stories.js');
 require('./StatusComponent.stories.js');
 
 storiesOf('Metamask', module)
-  .add('accounts not available', () => <Metamask accounts={[]} web3={{ status: 'initialized' }} />)
+  .add('accounts not available', () => (
+    <Metamask accounts={[]} web3={{ status: 'initialized' }} />
+  ))
   .add('failed', () => <Metamask accounts={[]} web3={{ status: 'failed' }} />);
