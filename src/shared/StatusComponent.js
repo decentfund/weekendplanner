926608f89da2117002f49e01f83a2f9cec7a37c2
palevoo <kud.nikita@gmail.com>
2018-05-19 14:12:00 +0200

    address validator, 2 more stories added

--- src/shared/StatusComponent.js
+++ src/shared/StatusComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Blockies from 'react-blockies';
+// import { connect } from 'react-redux';
+
 import validAddress from '../util/validAddress';
 
-export const clipper = address => {
+export const clipper = (address) => {
   if (!validAddress(address)) return 'Invalid address';
   const arr = address.split('');
   const first = arr.slice(0, 4).join('');
@@ -11,25 +13,63 @@ export const clipper = address => {
   return `${first}...${last}`;
 };
 
-const StatusComponent = props => (
+// const { seed, size, scale, color } = props;
+// const { seed, ...otherProps } = props;
+
+const StatusComponent = ({
+  seed,
+  size,
+  scale,
+  color,
+  bgColor,
+  spotColor,
+  address,
+  vote,
+  owner,
+}) => (
   <div>
-    <Blockies {...props} />
-    <div>{clipper(props.address)}</div>
-    <div>{props.vote}</div>
-    <div>{props.owner}</div>
+    <Blockies
+      seed={seed}
+      size={size}
+      scale={scale}
+      color={color}
+      bgColor={bgColor}
+      spotColor={spotColor}
+    />
+    <div>{clipper(address)}</div>
+    <div>{vote}</div>
+    <div>{owner}</div>
   </div>
 );
 
 StatusComponent.defaultProps = {
-  address: null,
   vote: null,
   owner: true,
+  seed: 'weekendplanner',
+  size: 10,
+  scale: 3,
+  color: '#dfe',
+  bgColor: '#ffe',
+  spotColor: '#abc',
 };
 
 StatusComponent.propTypes = {
-  address: PropTypes.string,
+  address: PropTypes.string.isRequired,
   vote: PropTypes.string,
   owner: PropTypes.string,
+  seed: PropTypes.string,
+  size: PropTypes.number,
+  scale: PropTypes.number,
+  color: PropTypes.string,
+  bgColor: PropTypes.string,
+  spotColor: PropTypes.string,
 };
 
-export default StatusComponent;
+const mapStateToProps = (state, ownProps) => ({
+  owner: state.accounts[0] === ownProps.address,
+  // vote:
+});
+
+export { StatusComponent };
+// import { StatusComponent }
+// export default connect(mapStateToProps)(StatusComponent)
--- stories/StatusComponent.stories.js
+++ stories/StatusComponent.stories.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import StatusComponent from '../src/shared/StatusComponent';
+import { StatusComponent } from '../src/shared/StatusComponent';
 
 storiesOf('StatusComponent', module)
   .add('default', () => (
-    <StatusComponent
-      seed="Jeremy"
-      size={10}
-      scale={3}
-      color="#dfe"
-      bgColor="#ffe"
-      spotColor="#abc"
-      address="0xba7590098ad09ca35fde9ede64e58b72552bb10c"
-      vote="yes"
-    />
+    <StatusComponent address="0xba7590098ad09ca35fde9ede64e58b72552bb10c" vote="yes" />
   ))
   .add('owner', () => (
     <StatusComponent
-      seed="Alex"
-      size={10}
-      scale={3}
-      color="#dfed3d"
-      bgColor="#3g3f5"
-      spotColor="#ad4fbc"
       address="0xba7590098ad09ca35fde9ede64e58b72552bb10c"
       vote="no"
       owner="it's you"
+    />
+  ))
+  .add('invalid address', () => (
+    <StatusComponent address="0xba7590098ad09ca35fde9ede64e2552bb10c" />
+  ))
+  .add('passing explicit props', () => (
+    <StatusComponent
+      address="0xba7590098ad09ca35fde9ede64e58b72552bb10c"
+      vote="yes"
+      owner="it's you"
+      seed="johnsnow"
+      size={20}
+      scale={3}
+      color="#fafafa"
+      bgColor="#ddd"
+      spotColor="#FB6D56"
     />
   ));
