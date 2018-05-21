d5f2791ccf65cba5320dcd2b59854d7faa328a14
palevoo <kud.nikita@gmail.com>
2018-05-21 11:43:23 +0200

    mend

--- src/shared/StatusComponent.js
+++ src/shared/StatusComponent.js
@@ -7,9 +7,6 @@ import validAddress from '../util/validAddress';
 
 export const clipper = (address) => {
   if (!validAddress(address)) return 'Invalid address';
-=======
-const clipper = address => {
->>>>>>> 56c8365... refactored
   const arr = address.split('');
   const first = arr.slice(0, 4).join('');
   const last = arr.slice(arr.length - 4).join('');
@@ -44,8 +41,6 @@ const StatusComponent = ({
     {owner ? <div>It&apos;s you</div> : null}
   </div>
 );
-=======
->>>>>>> 56c8365... refactored
 
 StatusComponent.defaultProps = {
   vote: null,
--- src/shared/StatusComponent.test.js
+++ src/shared/StatusComponent.test.js
@@ -3,15 +3,16 @@ import { shallow } from 'enzyme';
 import { clipper, mapStateToProps, StatusComponent } from './StatusComponent';
 
 const currentAccount = '0x5ed8cee6b63b1c6afce3ad7c92f4fd7e1b8fad9f';
+describe('Clipper', () => {
+  it('wrong address provided', () => {
+    const adrs = '0294949c595';
+    expect(clipper(adrs)).toBe('Invalid address');
+  });
 
-it('wrong address provided', () => {
-  const adrs = '0294949c595';
-  expect(clipper(adrs)).toBe('Invalid address');
-});
-
-it('clips address properly', () => {
-  const adrs = '0x5ed8cee6b63b1c6afce3ad7c92f4fd7e1b8fad9f';
-  expect(clipper(adrs)).toBe('0x5e...ad9f');
+  it('clips address properly', () => {
+    const adrs = '0x5ed8cee6b63b1c6afce3ad7c92f4fd7e1b8fad9f';
+    expect(clipper(adrs)).toBe('0x5e...ad9f');
+  });
 });
 
 const stateNoMetamask = {
@@ -30,18 +31,20 @@ const stateDifferentAccount = {
   },
 };
 
-it('sets props from mapStateToProps correctly if accounts is empty', () => {
-  const resultProps = mapStateToProps(stateNoMetamask, {
-    address: currentAccount,
+describe('Map to state owner', () => {
+  it('sets props from mapStateToProps correctly if accounts is empty', () => {
+    const resultProps = mapStateToProps(stateNoMetamask, {
+      address: currentAccount,
+    });
+    expect(resultProps.owner).toEqual(false);
   });
-  expect(resultProps.owner).toEqual(false);
-});
 
-it('sets props from mapStateToProps correctly if current account is owner', () => {
-  const resultProps = mapStateToProps(stateCurrentAccount, {
-    address: currentAccount,
+  it('sets props from mapStateToProps correctly if current account is owner', () => {
+    const resultProps = mapStateToProps(stateCurrentAccount, {
+      address: currentAccount,
+    });
+    expect(resultProps.owner).toEqual(true);
   });
-  expect(resultProps.owner).toEqual(true);
 });
 
 it('sets props from mapStateToProps correctly if current account is not owner', () => {
@@ -53,12 +56,14 @@ it('sets props from mapStateToProps correctly if current account is not owner', 
 
 const setupComponent = props => <StatusComponent owner={false} {...props} />;
 
-it("doesn't render owner div for the current user", () => {
-  const component = shallow(setupComponent());
-  expect(component.contains('It&aposs you')).toEqual(false);
-});
+describe('<StatusComponent />', () => {
+  it("doesn't render owner div for the current user", () => {
+    const component = shallow(setupComponent());
+    expect(component.contains('It&aposs you')).toEqual(false);
+  });
 
-it('does render owner div for the current user', () => {
-  const component = shallow(setupComponent({ owner: true }));
-  expect(component.contains("It's you")).toEqual(true);
+  it('does render owner div for the current user', () => {
+    const component = shallow(setupComponent({ owner: true }));
+    expect(component.contains("It's you")).toEqual(true);
+  });
 });
